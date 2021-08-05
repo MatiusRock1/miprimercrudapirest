ab8cd49e955ddb7d9fdf6d604e887573d6c5b251
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2021-08-05 16:46:10 -0500

    creacion de shema y configuracion del servidor express

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const morgan = require ('morgan');
+const mongoose = require ('mongoose');
+
+const app = express();
+
+//setings
+app.set('port', process.env.PORT|| 3000);
+//mongose
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex',true);
+mongoose.set('useUnifiedTopology' , true);
+
+mongoose.connect('mongodb+srv://MatiusRock:3CASL3u1Kpibt7Yi@cluster0.xglab.mongodb.net/myFirstDatabaseApiRest?retryWrites=true&w=majority')
+.then(db => console.log('db conectada'))
+.catch(err => console.log(err));
+
+const indexRouters = require('./routers/index')
+app.use('/',indexRouters);
+app.use(express.urlencoded({extended:false}));
+
+app.use(morgan('dev'));
+
+app.listen(app.get('port'),() =>{
+    console.log(`Server on port ${app.get('port')}`);
+})
+
+
+
---
+++ src/models/registros.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const moongose = require('mongoose');
+const Schema = moongose.Schema;
+
+const RegisterShema = new Schema({
+    name : String,
+    lastname : String,
+    birthdate : Date,
+    address : String,
+    status : {
+        type : Boolean,
+        default: true
+    }
+
+});
+
+module.exports = moongose.model('register' , RegisterShema);
