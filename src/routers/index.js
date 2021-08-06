30cbf387cc919ab833c48e80c48d2d9ca3f55378
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2021-08-06 14:47:49 -0500

    Get register

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const register = require ('../models/register');
 const Register = require ('../models/register');
 var RegisterShemavalidator = require('jsonschema').Validator;
 var RegisterShema = new RegisterShemavalidator();
@@ -8,17 +7,31 @@ var RegisterShema = new RegisterShemavalidator();
 const schema = require('../Schema/register.json');
 
 
+router.get('/register' , async (req,res) => {
+  try { 
+  const register = await Register.find();
+  res.json(register);  
+  }
+  catch (err)
+  {
+    console.log(err);
+    res.json({response: 1});
+  }
+});
 
 
 router.post('/register' , async (req,res) => {
-     const register = new Register(req.body);
+      const register = new Register(req.body);
      const validation = (RegisterShema.validate(req.body, schema));
      if(validation.errors.length == 0 ){
-      await register.save()
-      .then(db => console.log('actualizacion correcta de datos'))
-      .catch(err => console.log(err));
-      ;
+      try { 
+      await register.save();
       res.json({response: 0});
+      }
+      catch (err){
+        console.log(err);
+        res.json({response: 1});
+      }
      }
      else{
        res.json(validation.errors);
